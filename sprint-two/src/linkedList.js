87cce38725e35cac7f39a6b9aad6906e28c4a6b5
nkbelow <nkbelow@gmail.com>
2017-02-09 10:42:54 -0800

    finished addToTail method in Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,6 +4,15 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //new node creates a node connected to previous node
+    //new node becomes tail node
+    var node = new Node(value);
+    if(list.head === null){
+      list.head = node;
+    } else {
+      list.tail.next = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function() {
