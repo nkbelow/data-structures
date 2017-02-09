f974d885a94f1cf0a2a870003bf706876748fca4
nkbelow <nkbelow@gmail.com>
2017-02-08 16:19:14 -0800

    Made all functional pattern tests pass and stack functional shared

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,6 +1,7 @@
 var Queue = function() {
   var someInstance = {};
   var count = 0;
+  var currentTopIndex = 0;
 
 
   // Use an object with numeric keys to store values
@@ -9,13 +10,23 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[currentTopIndex] = value;
+    count++;
+    currentTopIndex++;
   };
 
   someInstance.dequeue = function() {
+    if(count !== 0){
+      count--;
+    }
+    var firstOut = storage[Math.min.apply(null, Object.keys(storage))];
+    console.log(firstOut);
+    delete storage[Math.min.apply(null, Object.keys(storage))];
+    return firstOut;
   };
 
   someInstance.size = function() {
-    return 0;
+    return count;
   };
 
   return someInstance;
